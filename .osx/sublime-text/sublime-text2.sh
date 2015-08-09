a8eee972e03290c60d72f38df732fa5286aba1bf
will <githubutilities@163.com>
2015-08-09 15:38:57 +0800

    Add package-control install script

--- .osx/sublime-text/sublime-text2.sh
+++ .osx/sublime-text/sublime-text2.sh
@@ -5,7 +5,20 @@ cd "$(dirname "${BASH_SOURCE}")";
 SUBLIME_SETTING_GIT_REPOSITORY=https://github.com/githubutilities/sublime2_user_settings.git
 TARGET_SUBLIME_SETTING_DIRECTORY=~/Library/"Application Support"/"Sublime Text 2"/Packages/User
 
-function doIt() {
+# Sublime text package control config variable
+SUBLIME_PACKAGE_CONTROL_URL_PATH=https://packagecontrol.io/Package%20Control.sublime-package
+SUBLIME_PACKAGE_CONTROL_FILE_PATH=~/Library/"Application Support"/"Sublime Text 2"/"Installed Packages"/"Package Control.sublime-package"
+
+function installPackageControl() {
+	if [ ! -e "${SUBLIME_PACKAGE_CONTROL_FILE_PATH}" ]; then
+		echo "Installing Package Control";
+		curl -fsSL $SUBLIME_PACKAGE_CONTROL_URL_PATH --output "${SUBLIME_PACKAGE_CONTROL_FILE_PATH}";
+	else
+		echo "Package Control already installed";
+	fi;
+}
+
+function downloadSublimeSettings {
 	if [ ! -d "${TARGET_SUBLIME_SETTING_DIRECTORY}" ]; then
 		git clone $SUBLIME_SETTING_GIT_REPOSITORY "${TARGET_SUBLIME_SETTING_DIRECTORY}" 2>&1 1> /dev/null;
 	else
@@ -20,6 +33,13 @@ function doIt() {
 			cd -;
 		fi;
 	fi;
+}
+
+function doIt() {
+	echo "";
+	installPackageControl;
+	echo "";
+	downloadSublimeSettings;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -37,6 +57,10 @@ else
 	fi;
 fi;
 
+unset installPackageControl;
+unset downloadSublimeSettings;
 unset doIt;
 unset SUBLIME_SETTING_GIT_REPOSITORY;
 unset TARGET_SUBLIME_SETTING_DIRECTORY;
+unset SUBLIME_PACKAGE_CONTROL_URL_PATH;
+unset SUBLIME_PACKAGE_CONTROL_FILE_PATH;
