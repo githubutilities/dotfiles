f12ce22d3209ee5e9b64df59064d3cb820bed176
will <githubutilities@163.com>
2016-01-19 22:23:14 +0800

    Add activator cheatsheet

---
+++ .ios/cheatsheet/activator.sh
@@ -0,0 +1,17 @@
+# List activator listeners
+activator listeners
+
+# Sleep Button
+activator send libactivator.system.sleepbutton
+
+# Unlock screen
+activator send libactivator.lockscreen.toggle
+
+# Press Home Button
+activator send libactivator.system.homebutton
+#stouch swipe 200 300 700 300 0.2 1
+
+# Reference
+#* http://stackoverflow.com/questions/28196778/open-app-with-open-command-in-locked-screen-ios-8
+#* [Activator Listeners](http://junesiphone.com/actions/)
+
