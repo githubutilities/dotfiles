08da68e85bc18366f46b5048e397f5cf95220ce6
will <githubutilities@163.com>
2016-02-19 10:03:44 +0800

    Add reference to secure delete install script

--- .ubuntu/secure-delete/install.sh
+++ .ubuntu/secure-delete/install.sh
@@ -11,6 +11,11 @@ sudo apt-get install --yes nautilus-wipe
 # A GUI Disk Utility
 sudo apt-get install --yes bleachbit
 
-# Use shred instead, checkout `cheat shred` and http://askubuntu.com/questions/57572/how-to-delete-files-in-secure-manner
+# If using `shred` instead of `srm`, you can refer to `cheat shred` or http://askubuntu.com/questions/57572/how-to-delete-files-in-secure-manner
 # * A guide for full disk encryption with ubuntu - https://thesimplecomputer.info/full-disk-encryption-with-ubuntu
 # * Encrypt Home Folder in Ubuntu with `ecryptfs-utils` and `cryptsetup` - http://askubuntu.com/questions/366749/enable-disk-encryption-after-installation
+
+# There is two level encryption(Please refer to http://www.cyberciti.biz/hardware/howto-linux-hard-disk-encryption-with-luks-cryptsetup-command/):
+# 1. Filesystem stacked level encryption
+# 2. Block device level encryption
+
